ed4bfa2f57280f41136716efdb7e0222b50bf3d6
Ryan <ryan.gooler@gmail.com>
2019-12-02 23:45:01 -0600

    Creating Skeleton

---
+++ 0/main.py
@@ -0,0 +1,17 @@
+class challenge:
+    def part1(self):
+        return None
+
+    def part2(self):
+        return None
+
+if __name__ == "__main__":
+    chall = challenge()
+    with open('input') as fh:
+        lines = fh.readlines()
+        for line in lines:
+            part1 = part1 + chall.part1()
+            part2 = part2 + chall.part2()
+
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
