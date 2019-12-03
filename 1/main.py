b374e85b9dd1de04d9e20c660044617030ce392a
Ryan <ryan.gooler@gmail.com>
2019-12-02 21:47:52 -0600

    Day 1 done

--- 1/main.py
+++ 1/main.py
@@ -4,14 +4,23 @@ class challenge:
 
     def calc2(self, weight):
         # This time with recursion for fuel weight
-        return int(weight / 3) - 2
+        total = self.calc(weight)
+        fuelweight = self.calc(total)
+        while fuelweight > 0:
+            total = total + fuelweight
+            fuelweight = self.calc(fuelweight)
+        return total
+
 
 if __name__ == "__main__":
-    total = 0
+    part1 = 0
+    part2 = 0
     chall = challenge()
     with open('input') as fh:
         lines = fh.readlines()
         for line in lines:
-            total = total + chall.calc2(int(line))
+            part1 = part1 + chall.calc(int(line))
+            part2 = part2 + chall.calc2(int(line))
 
-    print(total)
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
