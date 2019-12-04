475e88a2e37e3f782bc1f7520a91b54e1ff5aac0
Ryan <ryan.gooler@gmail.com>
2019-12-03 23:11:32 -0600

    part 1

--- 4/main.py
+++ 4/main.py
@@ -4,11 +4,33 @@ class challenge:
     def load(self, data):
         self.data = [a.split(',') for a in data]
 
-    def part1(self):
-        return None
+    def part1(self, start=108457, end=562041):
+        counter = 0
+        for i in range(start, end):
+            if self.test(i):
+                counter = counter + 1
+        return counter
 
     def part2(self):
         return None
+
+    def test(self, i):
+        i = str(i)
+        # Contains double
+        if not any(map(i.__contains__, ['11','22','33','44','55','66','77','88','99','00'])):
+            return False
+        # Increasing order
+        if int(i[1]) < int(i[0]):
+            return False
+        if int(i[2]) < int(i[1]):
+            return False
+        if int(i[3]) < int(i[2]):
+            return False
+        if int(i[4]) < int(i[3]):
+            return False
+        if int(i[5]) < int(i[4]):
+            return False
+        return True
 
 def main():
     chall = challenge()
--- 4/test.py
+++ 4/test.py
@@ -2,10 +2,17 @@ from main import challenge
 import unittest
 
 class ChallengeTest(unittest.TestCase):
-    def test_part1(self):
+    def test_part1a(self):
         chall = challenge()
-        chall.load([])
-        self.assertEqual(chall.part1(), None)
+        self.assertEqual(chall.test('111111'), True)
+
+    def test_part1b(self):
+        chall = challenge()
+        self.assertEqual(chall.test('223450'), False)
+
+    def test_part1c(self):
+        chall = challenge()
+        self.assertEqual(chall.test('123789'), False)
 
     def test_part2(self):
         chall = challenge()
