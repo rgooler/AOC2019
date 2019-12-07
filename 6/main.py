a4c50b736f058075a1ad35e6755e0e0c7ca627d0
Jippen <lab@legitemail.xyz>
2019-12-06 21:46:40 -0600

    Day 6 complete

--- 6/main.py
+++ 6/main.py
@@ -1,14 +1,54 @@
+#!/usr/bin/env python 
+
+from graph_tool.all import *
+
 class challenge:
     data = []
 
     def load(self, data):
-        self.data = [a.split(',') for a in data]
+        data = [x.strip() for x in data]
+        # Create a new undirectedgraph
+        self.graph = Graph(directed=False)
 
+        # Label everything so I'm not going back and forth between hashes
+        v_name = self.graph.new_vertex_property('string')
+        self.graph.vertex_properties["name"] = v_name
+
+        self.planets = dict()
+        # Create all the vertexes first, so that creating the edges (orbits)
+        # is easier 
+        for item in data:
+            # Add vertexes, as needed
+            src, dest = item.split(")")
+            if src not in self.planets:
+                v_src = self.graph.add_vertex()
+                v_name[v_src] = src
+                self.planets[src] = v_src
+            if dest not in self.planets:
+                v_dest = self.graph.add_vertex()
+                v_name[v_dest] = dest
+                self.planets[dest] = v_dest
+            # Add edge
+            self.graph.add_edge(self.planets[src],self.planets[dest])
+               
     def part1(self):
-        return None
+        total = 0
+        for planet in self.planets:
+            total += self.to_COM(planet)
+        return total
 
     def part2(self):
-        return None
+        return self.calc_distance('YOU', 'SAN')
+
+    def to_COM(self, src):
+        return self.calc_distance(src, 'COM') - 2
+
+    def calc_distance(self, src, dest):
+        return shortest_distance(
+            self.graph,
+            self.planets[src],
+            self.planets[dest]
+            )
 
 def main():
     chall = challenge()
--- 6/test.py
+++ 6/test.py
@@ -1,7 +1,33 @@
+#!/usr/bin/env python 
+
 from main import challenge
 import unittest
 
 class ChallengeTest(unittest.TestCase):
+    def test_a(self):
+        chall = challenge()
+        data = ["COM)B"]
+        chall.load(data)
+        self.assertEqual(chall.part1(), 1)
+
+    def test_b(self):
+        chall = challenge()
+        data = ["COM)B", "B)C"]
+        chall.load(data)
+        self.assertEqual(chall.part1(), 3)
+
+    def test_c(self):
+        chall = challenge()
+        data = ["COM)B", "B)C", "B)D"]
+        chall.load(data)
+        self.assertEqual(chall.part1(), 5)
+
+    def test_d(self):
+        chall = challenge()
+        data = ["COM)B", "B)C", "C)D"]
+        chall.load(data)
+        self.assertEqual(chall.part1(), 6)
+
     def test_part1(self):
         chall = challenge()
         data = ["COM)B","B)C","C)D","D)E","E)F","B)G","G)H","D)I","E)J","J)K","K)L"]
